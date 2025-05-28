273effed0ee314c69085495d0be77c7aab9bbfba
Eric Hankinson <erichankinson@mac.lan>
2025-05-27 18:35:49 -0700

    Update app.js changes

--- pages/app.js
+++ pages/app.js
@@ -1,9 +1,9 @@
 import React, { useState, useRef, useEffect } from 'react';
-import Header from '../components/Header.js';
-import Navigation from '../components/Navigation.js';
-import Dashboard from '../components/Dashboard.js';
+import Header from '../components/header.js';
+import Navigation from '../components/navigation.js';
+import Dashboard from '../components/dashboard.js';
 import ChatInterface from '../components/ChatInterface.js';
-import Contracts from '../components/Contracts';
+import Contracts from '../components/contracts';
 import WordDocument from '../components/WordDocument.js';
 import AdminPanel from './adminpanel';import { isAdmin } from '../data/users';
 
